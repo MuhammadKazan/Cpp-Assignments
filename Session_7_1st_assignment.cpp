5d0c474079d885aa7408f4c86d9cdd5b2204a6b4
MuhammadKazan <115987993+MuhammadKazan@users.noreply.github.com>
2022-11-10 17:39:58 +0200

    Add files via upload

---
+++ Session_7_1st_assignment.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+void sortby (int a, int arr[], int n){
+   if(n==0){
+      for(int j=0; j<a;j++){
+         for(int i=0;i<a-1;i++){
+            if(arr[i]<arr[i+1]){
+               int temp=arr[i];
+               arr[i]=arr[i+1];
+               arr[i+1]=temp;
+            }
+         }
+      }
+   }
+   else if (n==1){
+      for(int j=0; j<a;j++){
+         for(int i=0;i<a-1;i++){
+            if(arr[i]>arr[i+1]){
+               int temp=arr[i];
+               arr[i]=arr[i+1];
+               arr[i+1]=temp;
+            }
+         }
+      }
+   }
+   else{
+      cout<<"Error! The sorting method input can only be 1 or 0."<<endl;
+   }
+}
+
+int main(){
+   int NumberofElements, array[NumberofElements];
+   int Ascending;
+
+   cout<<"Enter the number of elements in the array: ";
+   cin>>NumberofElements;
+   cout<<"Enter the elements of the array: ";
+   for(int i=0; i<NumberofElements; i++){
+        cin>>array[i];
+   }
+
+   cout<<"Enter the sorting method (1=ascending, 0=descending): ";
+   cin>>Ascending;
+
+   sortby(NumberofElements,array,Ascending);
+
+   cout<<"Your array after sorting is: "<<endl;
+   for(int i=0;i<NumberofElements;i++){
+      cout<<array[i]<<"\t";
+   }
+
+   return 0;
+}
---
+++ Session_7_2nd_assignment.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+void reverse(int a, int arr[]){
+  for(int i=0; i<a-1;i++){
+        int temp=arr[i];
+        arr[i]=arr[a-1];
+        arr[a-1]=temp;
+        a--;
+    }
+}
+
+int main(){
+    int  NumberofElements, array[NumberofElements];
+    
+    cout<<"Enter the number of elements in your array: ";
+    cin>>NumberofElements;
+    cout<<"Enter the elements of the array: ";
+    for(int i=0; i<NumberofElements; i++){
+        cin>>array[i];
+    }
+
+   reverse(NumberofElements,array);
+
+    cout<<"Your array post-reversal is: "<<endl;
+    for(int i=0; i<NumberofElements; i++){
+        cout<<array[i]<<"\t";
+    }
+
+    return 0;
+}
